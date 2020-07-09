52391e244df8f3c42e47ea7f26bec577990a0855
vinnief <vincy@sapo.cv>
2020-07-09 10:50:20 +0200

    added Rmarkdown output for automated tabling. A version of Graphs now is a notebook.

--- Graphs.R
+++ Graphs.R
@@ -16,6 +16,8 @@ tim = Sys.time()
 walkThrough( lpdf = JHH, regions = JHHRegios, graphlist = myGraphNrs, myFolderDate  = 'current', ordre = 'RG')
 reportDiffTime('JHH graphs',tim,units = 'mins')
 
+
+
 #do a month
 walkThrough(lpdf = JHH, regions = JHHRegios, graphlist = myGraphNrs, from = "2020-01-01", to = '2020-01-31', myFolderDate = 'January', ordre = 'RG')
 walkThrough(lpdf = JHH, regions = JHHRegios, graphlist = myGraphNrs, from = "2020-04-01", to = '2020-04-30', myFolderDate = 'April', ordre = 'RG')
@@ -24,42 +26,3 @@ walkThrough(lpdf = JHH, regions = JHHRegios, graphlist = myGraphNrs, from = "202
 
 writeWithCounters(ECDC,name = "Covid19ECDC")
 writeWithCounters(JHH,name = "Covid19JHH") 
-#check what graphs are defined 
-#graphCodes()
-myGraphListbyDate
-myGraphListbyDay
-myGraphNrs
-myGraphList
-
-verbose = 2
-#look at growth rates and growth paths in first days (Synchronized)
-walkThrough(ECDC,regions = ECDCRegios,graphlist = c("graph1dnr_iyl","graph2dac_iyl"))
-
-#simulate deaths and confirmed   
-walkThrough(ECDC, ECDCRegios,ext = '_sim', graphlist = c('graphDccprr_fiyl','graphDddp_fyl'))  #sims
-walkThrough(ECDC , ECDCRegios,ext = '_endsim',graphlist = c('graphDccprr_fiyl','graphDddp_fyl')) #simulations 
-graphDddp_fyl(JHH,regios$Vincent,savename  = "deaths missed") 
-
-if ( weekdays( Sys.Date() , abbreviate = FALSE) == "Friday")   walkThrough(ECDC, ECDCRegios, graphlist = myGraphListbyDate)
-
-JHH %>% walkThrough(regions = JHHRegios,graphlist = 'graphDccprr_fyl')
-JHH %>% walkThrough(regions = JHHRegios[1:2],graphlist = 'graphDccprr_fyl')
-  #walkThrough(JHH, regions=JHHRegios[1:2],graphlist = 'graphDccprr_fyl')
-
-
-verbose = 1
-#do all months
-
-makeHistoryGraphs(JHH, regions = JHHRegios, fromDates = seq(as.Date('2020-01-01'),Sys.Date(),  by = '1 month'), ordre = 'GR')  
-makeHistoryGraphs(ECDC, regions = ECDCRegios, fromDates = seq(as.Date('2020-01-01'),Sys.Date(),  by = '1 month'))  
-startDate = "2020-01-01"
-fromDates <- as.character(seq(as.Date(startDate), length = 12, by = "1 month"))
-toDates <- as.character(seq(as.Date(fromDates[2]), length = 12, by = "1 month") - 1)
-
-makeHistoryGraphs(ECDC,ECDCRegios, graphlist = myGraphNrs, 
-                  fromDates = fromDates, toDates = toDates)
-
-makeHistoryGraphs(JHH,JHHRegios, graphlist = myGraphNrs, 
-                  fromDates = fromDates, toDates = toDates)
-
-
--- definitions.R
+++ definitions.R
@@ -6,22 +6,38 @@ LAGRD <- 36
 LAGDC <- LAGRC - LAGRD
 deathRate = .05
 if (!exists("verbose")) verbose <- 1
-
 myDateFormat <- "%Y-%m-%d"
-if (.Platform$OS.type == "unix")  {myPlotPath <- "~/Covid19_plots"
- } else myPlotPath <- "G:/My Drive/Covid19_plots"
- #R.version$os or Sys.info()["sysname"] 
+dataPath = './data'
+if (!dir.exists(dataPath)) dir.create(dataPath, recursive = TRUE)
 
-switch(Sys.info()[['sysname']],
-       Windows = {print("I'm a Windows PC.")},
-       Linux   = {print("I'm a penguin.")},
-       Darwin  = {print("I'm a Mac.")})
-myPath <- myPlotPath 
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)) {
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else {## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+switch(get_os(), 
+       windows = {print("I run MS Windows.");myPlotPath <- "G:/My Drive/Covid19_plots"},
+       linux   = {print("I'm a penguin."); myPlotPath <- "~/Covid19_plots"},
+       osx     = {print("I'm a Mac.");myPlotPath <- "~/Covid19_plots"},
+       ...     = {print('not recognized OS');myPlotPath <- "~/Covid19_plots"})
+.Platform$OS.type
+Sys.info()[['sysname']]
+R.version$os # mingw32 for win!
+
 if (!dir.exists(myPlotPath %//% 'data')) dir.create(myPlotPath %//% 'data', recursive = TRUE)
-datapath = './data'
-if (!dir.exists(datapath)) dir.create(datapath, recursive = TRUE)
 
-#data loading
+#define data loading
 #*Note*: the data of John hopkins, 
 #git@github.com:CSSEGISandData/COVID-19.git
 #before 20200325:#c <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
@@ -259,7 +275,7 @@ makelpdfUSStates <- function(){
  wc <- readUSdata('confirmed') 
  #geo.location <- wc[, c("Combined_Key", "Country_Region", "Province_State", "Admin2", "UID", "Lat", "Long_")]
  wc <- correctnames(wc)
- #write.csv( geo.location, file = datapath %#% "/" %#% "geo.location.US.csv", na = "")
+ #write.csv( geo.location, file = dataPath %#% "/" %#% "geo.location.US.csv", na = "")
  #rm(geo.location)
  # wc <- wc[, !names(wc) %in% c("Admin2" , "UID", "iso2", "iso3", "code3", "FIPS")]
  confirmed <- convertdata(wc, values.name = "confirmed", US = TRUE)
@@ -293,7 +309,7 @@ makelpdfUS <- function() {
 makelpdf <- function() {
  wc <- readdata('confirmed') #"Confirmed")
  geo.location <- wc[c("Country.Region", "Province.State", "Lat", "Long")]
- #write.csv( geo.location, file = datapath %#% "/" %#% "geo.location.csv", na = "")
+ #write.csv( geo.location, file = dataPath %#% "/" %#% "geo.location.csv", na = "")
  confirmed <- convertdata(wc, values.name = "confirmed")
  wd <- readdata("deaths")
  deaths <- convertdata(wd, values.name = "deaths")
@@ -319,9 +335,9 @@ updateJHHFromWeb <- function(nameUS = "JHH_US.csv", namenonUS = "JHH_non_US.csv"
  }
 
 readLocalData <- function(nameUS = "JHH_US.csv", namenonUS = "JHH_non_US.csv"){
- #CUS <- read.csv(datapath %#% "/" %#% nameUS, stringsAsFactors = FALSE)#colClasses = ("Date" = "character"))
+ #CUS <- read.csv(dataPath %#% "/" %#% nameUS, stringsAsFactors = FALSE)#colClasses = ("Date" = "character"))
  CUS2 <- read_csv(nameUS)
- #Cworld <- read.csv(datapath %#% "/" %#% namenonUS, stringsAsFactors = FALSE)
+ #Cworld <- read.csv(dataPath %#% "/" %#% namenonUS, stringsAsFactors = FALSE)
  Cworld2 <- read_csv(namenonUS)
  #lpdf <- rbind(Cworld, CUS)
  lpdf2 <- rbind(Cworld2, CUS2)
@@ -378,7 +394,7 @@ makeJHH <- function(name = "JHH", force = FALSE) {
   if (verbose >= 1) print("updating JHH from Github")
  } else {
   if (verbose >= 1) print(paste("loading local", namedays))
-  lpdf <- read.csv(datapath %#% "/" %#% namedays, stringsAsFactors  = FALSE)
+  lpdf <- read.csv(dataPath %#% "/" %#% namedays, stringsAsFactors  = FALSE)
  }
  if (typeof(lpdf$Date)  == "character") 
   lpdf$Date <- as.Date(lpdf$Date, "%Y-%m-%d") #strptime gives timezones! no need for timezones
@@ -560,11 +576,11 @@ makeRegioList <- function(lpti = JHH, piecename = "JHH"){
 }
 
 addPopulation <- function(lpdf) {
- population <- read.csv(datapath %#% "/" %#% 'population.csv')[c(1, 3)]
+ population <- read.csv(dataPath %#% "/" %#% 'population.csv')[c(1, 3)]
  names(population)[2] <- "population"
  rownames(population) <- population$Country.Name
  lpdf[, "population"] <- population[lpdf  %>%  pull(PSCR), "population"]
- popUS <- read.csv(datapath %#% "/" %#% 'USstatespop2019.csv')[c('State', 'p2019')]
+ popUS <- read.csv(dataPath %#% "/" %#% 'USstatespop2019.csv')[c('State', 'p2019')]
  names(popUS)[2] <- "population"
  rownames(popUS) <- popUS$State
  lpdf[grepl(",US", lpdf$PSCR), "population"] <- 
@@ -998,7 +1014,7 @@ writeWithCounters <- function(lpdf = JHH, varname = "confirmed", ID = "PSCR", na
                 counter = makecountname("day", minv))
   }
   filename = paste(name, "days.csv", sep = "_")
-  write_csv(lpdf, path = datapath %#% "/" %#% filename, na = "")
+  write_csv(lpdf, path = dataPath %#% "/" %#% filename, na = "")
   write_csv(lpdf, path = myPlotPath %//% "data" %//% filename, na = "")
   if (verbose >= 1) print(paste("Written the current data with counters to disk as", filename, "for use in Tableau or Excel"))
 }
@@ -1144,7 +1160,7 @@ graphit_nocheck_for_single_point <- function(lpti, countries, minVal = 1, ID  = 
   if (verbose >=  4) print("graphit making plot"  % %  myFolderType %#% "/" %#% mytitle)
   myplot <- myplot + theme(text = element_text(size  = 20), 
   axis.text  = element_text(color  = "blue", size  = rel(.8)) )
-  if (myFolderType  !=  "") myPath <- myPath %//% myFolderType
+  if (myFolderType  !=  "") myPath <- myPlotPath %//% myFolderType
   if (!dir.exists(myPath)) dir.create(myPath, recursive  = TRUE)
   suppressWarnings(#options(warn = -2)
   png(filename  = myPath %//% myFilename %#% ".png", width  = 1600, height  = 900)
@@ -1285,7 +1301,7 @@ graphit <- function(lpti, countries, minVal  = 1, ID  = "PSCR", xvar  = "Date",
     if (verbose >=  4) print("graphit making plot"  % %  myFolderType %#% "/" %#% mytitle)
     myplot <- myplot + theme(text = element_text(size  = 20), 
                              axis.text  = element_text(color  = "blue", size  = rel(.8)) )
-    if (myFolderType  !=  "") myPath <- myPath %//% myFolderType
+    if (myFolderType  !=  "") myPath <- myPlotPath %//% myFolderType else myPath <- myPlotPath
     if (!dir.exists(myPath)) dir.create(myPath, recursive  = TRUE)
     on.exit(while (!is.null(dev.list())) dev.off() )
     #suppressWarnings(#options(warn = -2)
@@ -1593,10 +1609,7 @@ makeHistoryGraphs <- function(lpdf, regions, graphlist = myGraphNrs,
     if (missing(toDates)) {
         fromDates = min(lpdf$Date)
         toDates  = max(lpdf$Date)
-    } else {fromDates <- as.character(as.Date(
-        toDates[1:(length(toDates) - 1)]) + 1)
-        toDates <- toDates[2:length(toDates)]
-    #rep(min(lpdf$Date, length(toDates)))} 
+    } else {fromDates <- as.character(rep(min(lpdf$Date), length(toDates)))} 
     }} else {if (missing(toDates)) {
       toDates <- as.character(as.Date(
                   fromDates[2:length(fromDates)]) - 1)
